6a273da36c93b6a7987a3d4ec02756dfdb7b220f
Leonardo J. Rivero Matos <leonardojrmatos@gmail.com>
2024-09-23 17:16:21 -0400

    main update

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import argparse
 from endpoints import get_github_user_events
 from display import display_events
+import json
 
 parser = argparse.ArgumentParser(description="Fetch GitHub user activity.")
 parser.add_argument("username", help="GitHub username to fetch events for")
 args = parser.parse_args()
 
 events = get_github_user_events(args.username)
+
+with open("data.json", "w") as data:
+    json.dump(events, data, indent=4)
 
 if events:
     print(f"Recent events for {args.username}:")
